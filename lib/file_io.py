9c1504bfd9054b2e038888c5a43e8b85b93c17da
Anthony Ngonde <anto.ngonde@gmail.com>
2024-07-04 08:59:16 +0300

    lab done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,24 @@
-def write_file(file_name, file_content):
-    pass
+from pathlib import Path
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+
+    file_path = Path(file_name).with_suffix('.txt')
+    with file_path.open('w') as file:
+        file.write(file_content)
+
+def append_file(file_name, file_content):
+    file_path = Path(file_name).with_suffix('.txt')
+    with file_path.open('a') as file:
+        file.write(file_content)
 
 def read_file(file_name):
-    pass
+    file_path = Path(file_name).with_suffix('.txt')
+    with file_path.open('r') as file:
+        return file.read()
+
+
+# Example usage:
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added")
+append_file(file_name="logfile", file_content="Log 2: 3 bananas subtracted")
+content = read_file(file_name="logfile")
+print(content)
